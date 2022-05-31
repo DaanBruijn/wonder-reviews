01bd6c841a7ef35f55612522e6f50888923031a7
ReyhanGe <334480@student.mboutrecht.nl>
2022-05-31 15:09:07 +0200

    commıt

--- front-page.php
+++ front-page.php
@@ -7,6 +7,9 @@ $content = get_the_content();
 
 render('views/templates/custom.php', compact('title', 'content'));
 
+$arguments = ['post_type' => 'news', 'numberposts' => 2, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
+$ListofReviews = get_posts();
+
 ?>
 
 <!DOCTYPE html>
@@ -16,61 +19,54 @@ render('views/templates/custom.php', compact('title', 'content'));
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
 </head>
 
 <body>
 
-
     <ul>
         <li><a class="active" href="#home">Home</a></li>
         <li><a href="http://wonder-reviews.local/overzichtspagina/">Overzichpagina</a></li>
-        <li><a href="#about">About</a></li>
+        <!-- <li><a href="#about">About</a></li> -->
     </ul>
     <br>
 
     <div>
         <img class="anafoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="test">
+        <h1 class="anatext">Welkom</h1>
     </div>
 
-    <h1>Recente Reviews</h1>
+    <h1 class="text">Recente Reviews</h1>
 
     <marquee>
-
         <div class="row">
             <div class="col-sm col-md col-lg col-xl col-xxl">
                 <div class="hizmetkutu">
                     <div class="kutu">
-                        <img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
+                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/maxresdefault.jpg" alt="">
                         <p>Attractiepark TOVERLAND </p>
                     </div>
                 </div>
                 <div class="hizmetkutu">
                     <div class="kutu">
-                        <img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
+                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/sddefault.jpg" alt="">
                         <p>BAGATELLE </p>
                     </div>
                 </div>
                 <div class="hizmetkutu">
                     <div class="kutu">
-                        <img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-                        <p>Slagharen </p>
+                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/indir.jpg" alt="">
+                        <p>Slagharen</p><p>   </p>
                     </div>
                 </div>
                 <div class="hizmetkutu">
                     <div class="kutu">
-                        <img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
+                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/walibiefault-1.jpg" alt="">
                         <p>Walibi Holland</p>
                     </div>
                 </div>
             </div>
         </div>
-
-
     </marquee>
-
-
 
 
     <!-- 
@@ -99,7 +95,5 @@ render('views/templates/custom.php', compact('title', 'content'));
         </div>
     </div> -->
 
-
 </body>
-
 </html>
--- template-custom.php
+++ template-custom.php
@@ -10,7 +10,6 @@ render('views/templates/custom.php', compact('title', 'content'));
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +18,7 @@ render('views/templates/custom.php', compact('title', 'content'));
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>Overzichpagina</title>
+  <title>Overzichtpagina</title>
 </head>
 
 <body>
@@ -27,14 +26,12 @@ render('views/templates/custom.php', compact('title', 'content'));
   <ul>
     <li><a href="http://wonder-reviews.local">Home</a></li>
     <li><a class="active" href="#overzichpagina">Overzichpagina</a></li>
-    <li><a href="#about">About</a></li>
+    <!-- <li><a href="#about">About</a></li> -->
   </ul>
 
   <h1 class="overtext">Overzichtspagina</h1>
 
-
   <div class="row">
-
     <div class="leftcolumn">
       <div class="card">
         <a href="#">
@@ -48,7 +45,6 @@ render('views/templates/custom.php', compact('title', 'content'));
           </div>
           <h4>Attractiepark TOVERLAND</h4>
           <h5>Tussen Venlo en Eindhoven ligt Attractiepark Toverland. Betreed een betoverende wereld vol avontuur, shows en spectaculaire attracties. Vlieg mee onder de vleugels van de spectaculaire wing coaster Fēnix en ervaar het betoverende gevoel van vliegen over de wateren van Avalon. Kom tot rust in bootjesattractie Merlin’s Quest of stap in de hoogste houten achtbaan van de Benelux.</h5>
-
         </a>
       </div>
 
@@ -64,11 +60,9 @@ render('views/templates/custom.php', compact('title', 'content'));
           </div>
           <h4>BAGATELLE</h4>
           <h5>Bagatelle is een groene oase vol attracties, niet alleen voor onverschrokken avonturiers, maar ook voor de minder dappere gasten.</h5>
-
         </a>
       </div>
     </div>
-
 
     <div class="rightcolumn">
       <div class="card">
@@ -78,6 +72,7 @@ render('views/templates/custom.php', compact('title', 'content'));
           <h5>In Attractiepark Slagharen rol je van het ene avontuur in het andere avontuur. Reis in een paar stappen van het Wilde Westen naar Mexico, vier carnaval in het ritmische New Orleans en ga op zoek naar goud in Yellowstone. </h5>
         </a>
       </div>
+
       <div class="card">
         <a href="#">
           <img class="photo1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/duzicegif-1650.gif" alt="gif">
@@ -85,6 +80,7 @@ render('views/templates/custom.php', compact('title', 'content'));
           <h5>Het vakantiepark en alle faciliteiten liggen midden in de prachtige natuur van Nationaal Park Hollandse Duinen. Duinrell biedt het hele jaar door familieplezier. Geniet van attracties en het Tikibad. In de omgeving vind je duin, strand en bos en kun je heerlijk shoppen.</h5>
         </a>
       </div>
+
       <div class="card">
         <a href="#">
           <img class="photo1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/ddrollercoaster.gif" alt="">
@@ -92,12 +88,8 @@ render('views/templates/custom.php', compact('title', 'content'));
           <h5>Walibi Holland is niet zomaar een pretpark, het is het spectaculairste attractiepark van Nederland! Een dagje uit naar Walibi is HARDGAAN, maar biedt veel meer dan alleen maar achtbanen. Het park staat namelijk boordevol spectaculaire attracties die het tot de perfecte dag uit maken voor jong en oud.</h5>
         </a>
       </div>
-
-
     </div>
   </div>
 
-
 </body>
-
 </html>

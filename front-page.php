f9edb81178c040dc5b712b6824ffa4189c079dd9
DaanBruijn <332951@student.mboutrecht.nl>
2022-06-01 15:12:50 +0200

    reworked home to be dynamic

--- front-page.php
+++ front-page.php
@@ -5,95 +5,38 @@ $content = get_the_content();
 
 render('views/templates/custom.php', compact('title', 'content'));
 
-$arguments = ['post_type' => 'news', 'numberposts' => 2, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
-$ListofReviews = get_posts();
+$arguments = ['post_type' => 'review', 'numberposts' => 4, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
+$reviews = get_posts($arguments);
 
-$archive = get_post_type_archive_link('review');
+// $archive = get_post_type_archive_link('review');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<h1 class="text">Recente Reviews</h1>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-
-    <ul>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="<?php echo $archive; ?>">Overzichtspagina</a></li>
-        <li><a href="#about">About</a></li>
-    </ul>
-    <br>
-
-    <div>
-        <img class="anafoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="test">
-        <h1 class="anatext">Welkom</h1>
-    </div>
-
-    <h1 class="text">Recente Reviews</h1>
-
-    <marquee>
-        <div class="row">
-            <div class="col-sm col-md col-lg col-xl col-xxl">
-                <div class="hizmetkutu">
-                    <div class="kutu">
-                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/maxresdefault.jpg" alt="">
-                        <p>Attractiepark TOVERLAND </p>
-                    </div>
-                </div>
-                <div class="hizmetkutu">
-                    <div class="kutu">
-                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/sddefault.jpg" alt="">
-                        <p>BAGATELLE </p>
-                    </div>
-                </div>
-                <div class="hizmetkutu">
-                    <div class="kutu">
-                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/indir.jpg" alt="">
-                        <p>Slagharen</p><p>   </p>
-                    </div>
-                </div>
-                <div class="hizmetkutu">
-                    <div class="kutu">
-                        <img class="kayanfoto" src="http://wonder-reviews.local/wp-content/uploads/2022/05/walibiefault-1.jpg" alt="">
-                        <p>Walibi Holland</p>
-                    </div>
+<div class="row">
+    <div class="col-sm col-md col-lg col-xl col-xxl">
+        <?php if($reviews): ?>
+            <?php foreach($reviews as $review): ?>
+            <?php 
+                $id = $review->ID;
+                $title = get_field('title', $id);
+                $score = get_field('score', $id);
+                $img = get_field('image', $id);
+                $location = get_field('location', $id);
+            ?>
+            
+            <div class="hizmetkutu">
+                <div class="kutu">
+                    <img class="kayanfoto" src="<?php echo $img['url']; ?>" alt="">
+                    <p><?php echo $title; ?></p>
+                    <p><?php echo $score; ?></p>
+                    <p><?php echo $location; ?></p>
+                    <a href="<?php echo get_permalink($review); ?>">Lees meer</a>
                 </div>
             </div>
-        </div>
-    </marquee>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+</div>
 
-
-    <!-- 
-
-    <div class="grid-container">
-        <div class="item1">
-            <img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-            <p>1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, alias? Ipsa distinctio autem doloremque</p>
-
-        </div>
-        <div class="item2"><img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-            <p>2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, alias? Ipsa distinctio autem doloremque</p>
-
-        </div>
-        <div class="item3"><img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-            <p>3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, alias? Ipsa distinctio autem doloremque</p>
-        </div>
-        <div class="item4">4<img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-            <p>1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, alias? Ipsa distinctio autem doloremque</p>
-        </div>
-        <div class="item5">5<img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-            <p>1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, alias? Ipsa distinctio autem doloremque</p>
-        </div>
-        <div class="item6">6<img class="foto1" src="http://wonder-reviews.local/wp-content/uploads/2022/05/Goliath-ALG-1.jpg" alt="">
-            <p>1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, alias? Ipsa distinctio autem doloremque</p>
-        </div>
-    </div> -->
-
-</body>
-</html>
